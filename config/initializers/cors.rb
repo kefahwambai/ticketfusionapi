4ef126c64d2e52483ddd1b7500c75fdf14a8312e
KefahWambai <kefahngwei@gmail.com>
2024-10-17 15:04:59 +0300

    fix: added production links in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins "https://ticketfusion-admin.vercel.app", "https://jtickets.vercel.app"
     #  "http://localhost:3010", "http://localhost:4040"
 
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      credentials: true
   end
 end
