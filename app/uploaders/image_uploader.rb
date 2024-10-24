cd37468502ba687a3bb4f3ceec9fca624fa8f444
KefahWambai <kefahngwei@gmail.com>
2024-10-24 11:10:50 +0300

    update: removed carrierwave.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,18 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
-  def extension_allowlist
-    %w[jpg jpeg gif png]
-  end
-
-  def filename
-    "#{secure_token}.#{file.extension}" if original_filename.present?
-  end
-
-  protected
-
-  def secure_token
-    @secure_token ||= SecureRandom.uuid
+  version :thumbnail do
+    process resize_to_fit: [100, 100]
   end
 end
-
--- config/initializers/carrierwave.rb
+++
@@ -1,9 +0,0 @@
-CarrierWave.configure do |config|
-  config.cache_storage = :file 
-  config.cloudinary_credentials = {
-    cloud_name: ENV['CLOUDINARY_CLOUD_NAME'],
-    api_key:    ENV['CLOUDINARY_API_KEY'],
-    api_secret: ENV['CLOUDINARY_API_SECRET']
-  }
-  config.storage = :cloudinary
-end
